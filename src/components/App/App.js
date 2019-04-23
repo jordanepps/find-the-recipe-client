35f5747cd9baf98e1c14b06ce2351626be0bf2b3
Jordan Epps <jordanepps@gmail.com>
2019-04-23 07:50:39 -0400

    Create empty home page component with route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import HomePage from '../../routes/HomePage/HomePage';
 import './App.css';
 
 class App extends Component {
 	render() {
-		return <div className="App" />;
+		return (
+			<div className="App">
+				<h1>App</h1>
+				<Switch>
+					<Route exact path={'/'} component={HomePage} />
+				</Switch>
+			</div>
+		);
 	}
 }
 
