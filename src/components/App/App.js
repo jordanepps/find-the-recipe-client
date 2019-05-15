cf0c03cf3232c2fc6ddb3b508de8c4594258b486
Jordan Epps <jordanepps@gmail.com>
2019-05-15 09:47:57 -0400

    Create a private favorites route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,9 +6,11 @@ import RecipeListPage from '../../routes/RecipeListPage/RecipeListPage';
 import LoginPage from '../../routes/LoginPage/LoginPage';
 import RegistrationPage from '../../routes/RegistrationPage/RegistrationPage';
 import RecipePage from '../../routes/RecipePage/RecipePage';
+import FavoritesPage from '../../routes/FavoritesPage/FavoritesPage';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import PublicOnlyRoute from '../Utils/PublicOnlyRoute';
+import PrivateRoute from '../Utils/PrivateRoute';
 import './App.css';
 
 export default class App extends Component {
@@ -23,6 +25,7 @@ export default class App extends Component {
 						<PublicOnlyRoute path="/register" component={RegistrationPage} />
 						<Route path="/recipe-list" component={RecipeListPage} />
 						<Route path="/recipe/:recipeId" component={RecipePage} />
+						<PrivateRoute path="/favorites" component={FavoritesPage} />
 					</Switch>
 				</IngredientsProvider>
 				<Footer />
---
+++ src/routes/FavoritesPage/FavoritesPage.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import './FavoritesPage.css';
+
+export default class FavoritesPage extends Component {
+	render() {
+		return <div>Favorite</div>;
+	}
+}
